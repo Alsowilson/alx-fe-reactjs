45f6e6c200f61afb5f805986f6759fd0b6a36ae2
Alsowilson <wilsonokoth45@gmail.com>
2025-09-05 18:04:14 +0300

    completed task 3 by adding UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
@@ -9,9 +10,16 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
-      <Footer />   
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
