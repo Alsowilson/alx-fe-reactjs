07b07560ec710ddf7944995e56131c0db16b78df
Alsowilson <wilsonokoth45@gmail.com>
2025-09-08 19:01:42 +0300

    Refactored to use Context API for user data

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,24 +3,24 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import UserContext from './UserContext';  
+import UserContext from './UserContext';
 
 function App() {
   const userData = {
     name: "Alice",
-    age: 25,
+    age: "25",
     bio: "Loves hiking and photography"
   };
 
   return (
     <UserContext.Provider value={userData}>
-    <div>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <UserProfile />
-      <Footer />
-    </div>
+      <div>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <UserProfile />
+        <Footer />
+      </div>
     </UserContext.Provider>
   );
 }
