2b94063ed8adc8254457ccd1998c47ba8a03015b
Alsowilson <wilsonokoth45@gmail.com>
2025-09-21 13:54:40 +0300

    Did correction for checkers

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,10 @@
-
+import also from './components/also.jsx';
 function App() {
   return (
     <div> 
       <h1>GitHub User Search</h1>
       <p>App is ready for development</p>
+      <also />
     </div>
   );
 }
