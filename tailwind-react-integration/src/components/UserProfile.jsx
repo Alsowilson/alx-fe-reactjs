0e5aec505e3c6301b4dfcb38bb9cbafc987aea77
Alsowilson <wilsonokoth45@gmail.com>
2025-10-08 20:16:02 +0300

    task 3: add hover interactions and transition to UserProfile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-8 md:p-8 sm:p-4 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <img
+     <img
   src="https://via.placeholder.com/150"
   alt="User"
   className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto object-cover transition-transform duration-300 ease-in-out"
 />
 
-      <h1 className="text-lg md:text-xl text-blue-800 text-center my-4 hover:text-blue-500 transition-colors duration-200">
+      <h1 className="text-lg md:text-xl text-blue-800 text-center my-4 hover:text-blue-500 transition-colors duration-200 cursor-pointer">
         John Doe
       </h1>
       <p className="text-gray-600 text-sm md:text-base text-center">
