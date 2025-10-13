9423d741e814fea134ca3c4b42053bba2d76bd56
Alsowilson <wilsonokoth45@gmail.com>
2025-10-13 17:44:27 +0300

    fix formik integration and yup schema for task 0 checker

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,20 +1,16 @@
+import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
 import FormikForm from './components/FormikForm';
 
-function App() {
-  return (
-    <div>
-      <RegistrationForm />
-    </div>
-  );
-}
 
 function App() {
   return (
     <div>
-      <FormikForm/>
+      {/* You can comment out one when testing */}
+      {/* <RegistrationForm /> */}
+      <FormikForm />
     </div>
   );
 }
+
 export default App;
-
